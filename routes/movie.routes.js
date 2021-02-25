296b85cff9566d449664c1a6f1ee3a62f62e0d0e
frtdkmn <frtdkmn@gmail.com>
2021-02-25 23:17:50 +0300

    Project Part 2

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { modelName } = require('../models/Movie');
 const router = express.Router();
 //Model
 const MovieModel = require('../models/Movie');
@@ -10,7 +11,42 @@ router.get('/', (req, res) => {
                     .catch((errorMsg)=>{res.json(errorMsg);});
 })
 
-//GET A MOVIE : .../api/movies/123 
+//GET ALL MOVIES : .../api/movies/listWithDirector
+router.get('/listWithDirector', (req, res) => {
+    MovieModel.aggregate(
+        [
+            {
+                $lookup:
+                        {
+                            from:'directors',
+                            localField:'director_id',
+                            foreignField:'_id',
+                            as:'director'
+                        }
+            }
+        ]
+    )
+                    .then((movieList)=>{res.json(movieList);})
+                    .catch((errorMsg)=>{res.json(errorMsg);});
+})
+
+//GET ALL MOVIES : .../api/movies/ 
+router.get('/between/:startYear/:endYear', (req, res) => {
+    const {startYear,endYear} = req.params;
+    //greater than or equal --- less than or equal 
+    MovieModel.find({year:{"$gte":parseInt(startYear),"$lte":parseInt(endYear)}})
+                    .then((movieList)=>{res.json(movieList);})
+                    .catch((errorMsg)=>{res.json(errorMsg);});
+})
+
+//GET ALL MOVIES : .../api/movies/top10
+router.get('/top10', (req, res) => {
+    MovieModel.find().sort({imdb_score:-1}).limit(10)
+                    .then((movieList)=>{res.json(movieList);})
+                    .catch((errorMsg)=>{res.json(errorMsg);});
+})
+
+//GET A MOVIE : .../api/movies/:movieId 
 router.get('/:movieId', (req, res,next) => {
     MovieModel.findById(req.params.movieId)
                     .then((movieList)=>{          
@@ -38,5 +74,29 @@ router.post('/', function (req, res) {
             .then((data)=>{res.json(data);})
             .catch((err)=>{res.json(err)});
 })
+//localhost:3000/api/movies/:movieId
+//XXXXXX.findByIdAndUpdate("_id",req.body).YYYYYYYYYYY
+router.put('/:movieId',(req,res,next)=>{
+    MovieModel.findByIdAndUpdate(req.params.movieId,req.body,{new:true})
+                    .then((data)=>{res.json(data)})
+                    .catch((err)=>{
+                        next({message:'The movie was not found.',code:99})
+                        res.json(err)
+                    })
+})
+//localhost:3000/api/movies/:movieId
+router.delete('/:movieId',(req,res,next)=>{
+    MovieModel.findByIdAndRemove(req.params.movieId)
+                .then((data)=>{res.json(data)})
+                .catch((err)=>{
+                    next({message:'The movie was not found.',code:99})
+                    res.json(err)
+                })
+})
 
+/*
+modelName.functionName(Filter,propObjs)[.SKIP-LIMIT-SORT]
+            .then((resultObj)=>{res.json(resultObj)})
+            .catch((resultError)=>{res.json(resultError)})
+*/
 module.exports = router;
